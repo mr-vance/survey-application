32abe614aa6851c041b6e5677ebc38c03a70b8ae
mr-vance <vansmuchongo@gmail.com>
2023-06-19 18:36:15 +0200

    final commit

--- survey-application/src/components/Screen1.js
+++ survey-application/src/components/Screen1.js
@@ -14,7 +14,9 @@ const Screen1 = ({ handleScreenChange }) => {
     <div className="container">
         <div className="jumbotron text-center">
       <h1 className="display-4">Survey Application</h1>
-      <p className="lead">Welcome to Screen 1</p>
+      <p className="lead">Welcome to Screen 1 </p>
+      <p>Our application is designed to collect valuable user lifestyle preference data. Our calculations are designed to provide you with meaningful and interesting results, including the total number of completed surveys, average age, age range, and preferences among participants. With these insights, you can discover trends and understand how your preferences compare to others.</p>
+
       <button className="btn btn-primary btn-lg mr-3" onClick={handleFillSurvey}>Fill out survey</button>
       <button className="btn btn-secondary btn-lg" onClick={handleViewResults}>View survey results</button>
     </div>
