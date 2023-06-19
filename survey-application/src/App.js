7dc665058f311aaab2db8a5691fdea3b7c5b0d4a
mr-vance <vansmuchongo@gmail.com>
2023-06-19 11:55:49 +0200

    added base code

--- survey-application/src/App.js
+++ survey-application/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Screen1 from './components/Screen1';
+import Screen2 from './components/Screen2';
+import Screen3 from './components/Screen3';
 import './App.css';
 
 function App() {
+  const [currentScreen, setCurrentScreen] = useState(1);
+
+  const handleScreenChange = (screenNumber) => {
+    setCurrentScreen(screenNumber);
+  };
+
+  const renderScreen = () => {
+    switch (currentScreen) {
+      case 1:
+        return <Screen1 />;
+      case 2:
+        return <Screen2 />;
+      case 3:
+        return <Screen3 />;
+      default:
+        return <Screen1 />;
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {renderScreen()}
     </div>
   );
 }
